a8fead4c47f0678c14e98a0da4cc5ae2841744c6
Asif Saif Ali <asif263848@gmail.com>
2023-09-30 16:21:57 +0530

    printing wave array

--- Demo.java
+++ Demo.java
@@ -1,15 +1,22 @@
 class Demo{
     public static void main(String[] args){
-        int[] arr={1,2,1};
-        int n= arr.length;
-        int[] sums=new int[2 * n];
-
-        for(int i=0;i<arr.length;i++){
-            sums[i]=arr[i];
-            sums[i+n]=arr[i] ;
+        int[] arr={1,2,3,4,4,3,2,1};
+        int n = 4;
+        int temp=0,j=n;
+        for(int i=1; i<arr.length-2;i++){
+            if((arr.length/2)<=i){
+                j++;
+            }
+            
+            temp = arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
         }
-        for(int k:sums)
-        System.out.println(k);
-    }
+        for(int k:arr){
+        System.out.print(k  + " ");
+            // System.out.println(arr.length);
+}
+        }
+
 }
     
--- TwoDArray.java
+++ TwoDArray.java
@@ -3,18 +3,26 @@ public class TwoDArray {
     public static void main(String[] args){
 
         int[][] arr = {
+
             {1,2,3,4},
             {5,6,7,8},
             {9,10,11,12}
         };
         
         int i,j;
-
-        for(i=0;i<arr.length;i++){
-            for(j=0;j<arr[i].length;j++){
-                System.out.print(arr[i][j] +" ");
+        
+        for(i=0;i<=arr.length;i++){
+            if(i%2==0){
+               for(j=0;j<arr.length;j++){
+                 System.out.print(arr[j][i] +" ");
+               }
+            }else{
+                for(j=arr.length-1;j>=0;j--){
+                    System.out.print(arr[j][i] +" ");
+                }
             }
             System.out.println();
-        }
+    }
+        // System.out.println(arr.length);
     }
 }
