f7e5b6674635910941653e9473d61437949cc936
Asif Saif Ali <asif263848@gmail.com>
2023-11-02 23:18:56 +0530

    finding kids with larger candies

---
+++ KidWithLargerCandy.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class kidWithLargerCandy{
+    public static void main(String[] args){
+        int [] candy = {12,1,12};
+        int extraCandy = 10;
+        System.out.println(findLargestNumber(candy,extraCandy));
+        }
+        /**
+         * @param arr
+         * @param extraCandy
+         * @return
+         */
+        public static List<Boolean> findLargestNumber(int[] arr, int extraCandy) {
+            int maxCandy = 0;
+            int count = 0;
+            List<Boolean> value = new ArrayList<Boolean>();
+            for(int i = 0; i < arr.length; i++){
+                maxCandy = arr[i] + extraCandy;
+                    for( int j = 0; j < arr.length; j++){
+                if(maxCandy >= arr[j]  ){
+                    count++;
+                  }else{
+                    count = 0;
+                  }
+                // count = maxCandy >= arr[j] ? count++ : 0;
+                }
+                if(count == arr.length) {
+                    value.add(true);
+                    count = 0;
+                }else{
+                    value.add(false); 
+                    count = 0;
+                } 
+                
+            }
+            return value;
+        }
+}
